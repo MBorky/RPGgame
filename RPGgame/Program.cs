3e60770bd3df3676853070911ec14b37e499dfad
borkowskimarcin <m2.borkowski@gmail.com>
2023-10-05 14:43:03 +0200

    NoChangeOnlyCommentToRememberSMTHNG

--- RPGgame/Program.cs
+++ RPGgame/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Primitive obsession
+// Value object
+// Atrybuty zamienic na klase - statystyki wrzucic do klasy jednej
+// Rozdzielic character od profession, character ma przetrzymywc profession
+// Character ma byc koordynacja wewnetrznych obiektow
+// klasa perki ma koordynowac dostep do perkow uzalezniony od dupereli
